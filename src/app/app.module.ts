8a4d92c19181697dd36671955d396fa6a21d7138
ganatanbriel <ganatanbriel@gmail.com>
2018-11-26 14:34:58 +0100

    initialization

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'angular-starter' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
