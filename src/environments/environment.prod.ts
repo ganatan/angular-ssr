cfd54a0ce826e6ff7bbc5cf1a13c3810e7a3bc0a
ganatan <dannyganatan@gmail.com>
2020-12-06 10:55:42 +0100

    Update Angular 11.0.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 11.0.2',
+    angular: 'Angular 11.0.3',
     bootstrap: 'Bootstrap 4.5.3',
     fontawesome: 'Font Awesome 5.15.1',
   }
