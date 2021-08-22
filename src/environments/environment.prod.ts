e4f259a56015bb8578e5cb02dd467238daf24cf2
ganatan <dannyganatan@gmail.com>
2021-08-22 16:56:41 +0200

    Update Angular 12.2.2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 12.1.3',
-    bootstrap: 'Bootstrap 5.0.2',
-    fontawesome: 'Font Awesome 5.15.3',
+    angular: 'Angular 12.2.2',
+    bootstrap: 'Bootstrap 5.1.0',
+    fontawesome: 'Font Awesome 5.15.4',
   }
 };
