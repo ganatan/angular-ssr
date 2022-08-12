d611fcef45cd0e81a7ba735a14e7334db697dc51
ganatan <dannyganatan@gmail.com>
2022-08-12 15:17:31 +0200

    Update Angular 14.1.2 & ESLint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-ssr',
-    version: 'Angular 14.1.1',
+    version: 'Angular 14.1.2',
     bootstrap: 'Bootstrap 5.2.0',
     fontawesome: 'Font Awesome 6.1.2',
   }
