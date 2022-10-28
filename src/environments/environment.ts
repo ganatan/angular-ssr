5b5519fc7a4c995b136c8eb2f15688a30bceedc8
ganatan <dannyganatan@gmail.com>
2022-10-28 13:44:28 +0200

    Update Angular 14.2.8

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-ssr',
-    version: 'Angular 14.2.5',
+    version: 'Angular 14.2.8',
     bootstrap: 'Bootstrap 5.2.2',
     fontawesome: 'Font Awesome 6.2.0',
   }
