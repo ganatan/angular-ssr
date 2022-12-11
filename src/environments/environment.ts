331e69872a8f2c75664e7324af8847e74f6ae5aa
ganatan <dannyganatan@gmail.com>
2022-12-11 11:50:16 +0100

    Update Angular 15.0.3 & add environment on angular.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-bootstrap',
-    version: 'Angular 15.0.1',
+    version: 'Angular 15.0.3',
     bootstrap: 'Bootstrap 5.2.3',
     fontawesome: 'Font Awesome 6.2.1',
   }
