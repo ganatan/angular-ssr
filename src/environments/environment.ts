7585a04a9c487491b5e6a2c59ee3f532fe3703cf
ganatan <dannyganatan@gmail.com>
2023-02-09 11:29:10 +0100

    Update Angular CLI 15.1.5 & fontawesome

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
     name: 'angular-bootstrap',
     version: 'Angular 15.1.2',
     bootstrap: 'Bootstrap 5.2.3',
-    fontawesome: 'Font Awesome 6.2.1',
+    fontawesome: 'Font Awesome 6.3.0',
   }
 };
