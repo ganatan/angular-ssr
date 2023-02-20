adf629acc82a349d7c2aafa10fa5c57a6e2d654c
ganatan <dannyganatan@gmail.com>
2023-02-20 09:01:23 +0100

    Update Angular 15.1.5 & Angular CLI 15.1.6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    version: 'Angular 15.1.3',
+    version: 'Angular 15.1.5',
     bootstrap: 'Bootstrap 5.2.3',
     fontawesome: 'Font Awesome 6.3.0',
   }
