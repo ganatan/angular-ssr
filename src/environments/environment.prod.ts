e1b768f11d8dab986e0a1f35edfc14a77217b26a
ganatan <dannyganatan@gmail.com>
2023-05-11 08:34:44 +0200

    Update Angular 16.0.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-ssr',
-    version: 'Angular 16.0.0',
+    version: 'Angular 16.0.1',
     bootstrap: 'Bootstrap 5.2.3',
     fontawesome: 'Font Awesome 6.4.0',
   }
