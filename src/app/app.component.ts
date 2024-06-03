4bbe4e15cd61eadac3e23b47d83b7c222468bf47
danny.collodet <danny.collodet.external@airbus.com>
2024-06-03 16:52:39 +0200

    fix document error on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,13 +17,21 @@ export class AppComponent implements OnInit {
   title = 'angular-routing';
   footerUrl = 'https://www.ganatan.com';
   footerLink = 'www.ganatan.com';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: object) {
+  }
+
   ngOnInit(): void {
 
-    const navMain = document.getElementById('navbarCollapse');
-    if (navMain) {
-      navMain.onclick = function onClick() {
-        if (navMain) {
-          navMain.classList.remove("show");
+    if (isPlatformBrowser(this.platformId)) {
+      const navMain = this.document.getElementById('navbarCollapse');
+      if (navMain) {
+        navMain.onclick = function onClick() {
+          if (navMain) {
+            navMain.classList.remove("show");
+          }
         }
       }
     }
