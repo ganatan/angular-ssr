88408291028edc4b34b02f5be0713ac704d69f67
danny.collodet <danny.collodet.external@airbus.com>
2024-06-12 14:38:16 +0200

    Update Angular 18.0.2 & Angular CLI 18.0.3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     application:
     {
       name: 'angular-bootstrap',
-      version: 'Angular 18.0.1',
+      version: 'Angular 18.0.2',
       bootstrap: 'Bootstrap 5.3.3',
       fontawesome: 'Font Awesome 6.5.2',
     }
