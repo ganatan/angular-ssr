494f25a4c3dd7dbeb1c55cb109077bbfd339266a
ganatan <dannyganatan@gmail.com>
2026-06-24 11:22:16 +0200

    Improve Angular 22.0.3

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(),
   ],
 };
